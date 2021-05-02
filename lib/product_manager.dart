071f296823ef13d756e1bdc7d37b97774d6d94bb
구본국 <kook9@gubongug-ui-Macmini.local>
2021-05-02 09:34:37 +0900

    Stateful Life Cycle 추가

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,17 +3,38 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  String startingProduct;
+
+  ProductManager(this.startingProduct) {
+    print('1:ProductsManager widget constructor');
+  }
+
   @override
   State<StatefulWidget> createState() {
+    print('2:ProductsManager State create');
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    print('3:ProductsManager State init');
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldwidget) {
+    print('5:ProductsManager State did update'); 
+    super.didUpdateWidget(oldwidget);
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('4:ProductsManager widget build');
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10.0), //margin
--- lib/products.dart
+++ lib/products.dart
@@ -4,18 +4,19 @@ class Products extends StatelessWidget {
   final List<String> products;
 
   //생성자  -- koo
-  Products(this.products);
+  Products(this.products) {
+    print('1:Products widget constructor');
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('2:Products widget build');
+
     return Column(
         children: products
             .map((element) => Card(
                   child: Column(
-                    children: <Widget>[
-                      Image.asset('food.jpg'),
-                      Text(element)
-                    ],
+                    children: <Widget>[Image.asset('food.jpg'), Text(element)],
                   ),
                 ))
             .toList());
