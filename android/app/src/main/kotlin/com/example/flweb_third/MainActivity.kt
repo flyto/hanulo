88eb2e78a47f8c3c991e8f0475e559d8a910299c
구본국 <kook9@gubongug-ui-Macmini.local>
2021-05-05 12:35:58 +0900

    커밋 코멘트

---
+++ android/app/src/main/kotlin/com/example/flweb_third/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flweb_third
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter 3번째',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: Container(
+        decoration : BoxDecoration(
+          gradient : LinearGradient(
+            begin : Alignment.centerLeft,
+            end : Alignment.centerRight,
+            colors : [Color.fromRGBO(195, 20, 50, 1),Color.fromRGBO(36, 11, 54, 1)]
+          ),
+        ),
+        child: Column(
+
+        )
+      ),
+    );
+  }
+}
